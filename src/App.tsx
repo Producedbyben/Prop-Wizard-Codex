184755701bd0ec506fd4313c4a8dd2e3f9c12abc
Producedbyben <ben@producedbyben.co.uk>
2026-07-15 12:29:05 +0100

    Support routing from the GitHub Pages base path

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App = () => (
       <AppProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter basename={import.meta.env.BASE_URL}>
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/project/:id" element={<ProjectPage />} />
